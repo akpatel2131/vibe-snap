ff630827852e00f4b7a7608f50a87f9b42299efc
krunal <krunal@krunals-MacBook-Air.local>
2024-12-16 18:12:28 +0530

    add netlify toml file

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -7,8 +7,6 @@ module.exports = {
     "stylelint:fix": "stylelint '**/*.{css,scss}' --fix",
   },
   rules: {
-    // "indentation": null,
-    // "string-quotes": null,
     "color-hex-length": "short",
     "block-no-empty": true,
     "selector-class-pattern": null,
